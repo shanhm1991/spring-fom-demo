b4e3cd44123bc8ce1b503257da527f4bac6feb79
shanhm1991@163.com <shanhm1991@163.com>
2021-12-16 17:05:41 +0800

    示例调整

---
+++ src/test/java/com/fom/test/schedule/TestTask.java
@@ -0,0 +1,21 @@
+package com.fom.test.schedule;
+
+import java.util.Random;
+
+import org.springframework.fom.Task;
+
+/**
+ * 
+ * @author shanhm1991@163.com
+ *
+ */
+public class TestTask extends Task<Long> {
+	
+	@Override
+	public Long exec() throws InterruptedException { 
+		long sleep = new Random().nextInt(10000);
+		logger.info("task executing ...");
+		Thread.sleep(sleep);
+		return sleep;
+	}
+}
